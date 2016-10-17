867c17a7a974cdc3e028712eb8e9c9e52888df54
Mentor <mentor@palokaj.co>
2016-10-17 12:38:15 +0200

    Funny status code

--- express-demo/app.js
+++ express-demo/app.js
@@ -2,7 +2,9 @@ const express = require( 'express' )
 const app	  = express()
 
 app.get('/', (request, response) => {
-	response.send( 'Hello Mentor, it is so good to see you in the browser!' )
+	response
+	.status(418)
+	.send( 'Hello Mentor, it is so good to see you in the browser!' )
 })
 
 app.listen( 8000, () => {
